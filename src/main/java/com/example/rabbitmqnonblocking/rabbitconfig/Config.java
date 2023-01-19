358f962ae334d980ec0aca0de9e79b825b4a73c9
Mahendra Bishnoi <mahendrabishnoi2@gmail.com>
2023-01-19 14:24:57 +0530

    connection block/un-block listener

--- src/main/java/com/example/rabbitmqnonblocking/rabbitconfig/Config.java
+++ src/main/java/com/example/rabbitmqnonblocking/rabbitconfig/Config.java
@@ -1,15 +1,21 @@
 package com.example.rabbitmqnonblocking.rabbitconfig;
 
+import java.io.IOException;
+
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.Connection;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionListener;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.amqp.RabbitProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+
+import com.rabbitmq.client.BlockedListener;
 
 @Configuration
 @ComponentScan
@@ -22,14 +28,32 @@ public class Config {
     }
 
     @Bean
-    public ConnectionFactory rabbitConnectionFactory(RabbitProperties rabbitProperties) {
+    public ConnectionFactory rabbitConnectionFactory() {
         com.rabbitmq.client.ConnectionFactory rabbitConnectionFactory = new com.rabbitmq.client.ConnectionFactory();
 //        rabbitConnectionFactory.setHost(rabbitProperties.getHost());
 //        rabbitConnectionFactory.setPort(rabbitProperties.getPort());
 //        rabbitConnectionFactory.setUsername(rabbitProperties.getUsername());
 //        rabbitConnectionFactory.setPassword(rabbitProperties.getPassword());
-        rabbitConnectionFactory.useNio();
+//        rabbitConnectionFactory.useNio();
         CachingConnectionFactory cachingConnectionFactory = new CachingConnectionFactory(rabbitConnectionFactory);
+        cachingConnectionFactory.addConnectionListener(new ConnectionListener() {
+            @Override
+            public void onCreate(Connection connection) {
+                connection.addBlockedListener(
+                        new BlockedListener() {
+                            @Override
+                            public void handleBlocked(String reason) {
+                                System.out.println("Connection blocked. Reason: " + reason);
+                            }
+
+                            @Override
+                            public void handleUnblocked() throws IOException {
+                                System.out.println("Connection unblocked.");
+                            }
+                        }
+                );
+            }
+        });
         return cachingConnectionFactory;
     }
 
